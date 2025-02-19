6fe6f4e58a3572334ab7e20d1a4757e7256693ef
Kulik Pavel <pashakulik58@gmail.com>
2025-02-19 13:27:03 +0700

    Поправил баг в первой лабе из-за которого клиент и сервер не связывались

--- client.c
+++ client.c
@@ -15,57 +15,46 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc < 4) {
-        printf("ВВЕСТИ client имя_хоста порт число\n");
-        return 1;
-    }
-
     int sock, number = atoi(argv[3]);
     socklen_t length;
     struct sockaddr_in servAddr, clientAddr;
+    struct hostent *hp, *gethostbyname();
     char buf[BUFLEN];
-
-    struct hostent* hp = NULL;
-
+    if (argc < 4) {
+        printf("ВВЕСТИ udpclient имя_хоста порт сообщение\n");
+        return 1;
+    }
     if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("He могу получить socket\n");
         return 1;
     }
-
-    if ((hp = gethostbyname(argv[1])) == NULL) {
-        perror("Хост не доступен или не существует\n");
-        return 1;
-    }
-
-    memcpy(&servAddr.sin_addr, hp->h_addr_list[0], hp->h_length);
+    bzero((char*)&servAddr, sizeof(servAddr));
     servAddr.sin_family = AF_INET;
+    hp = gethostbyname(argv[1]);
+    bcopy(hp->h_addr_list[0], &servAddr.sin_addr, hp->h_length);
     servAddr.sin_port = htons(atoi(argv[2]));
+    bzero((char*)&clientAddr, sizeof(clientAddr));
     clientAddr.sin_family = AF_INET;
+    clientAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     clientAddr.sin_port = 0;
-
     if (bind(sock, (struct sockaddr*)&clientAddr, sizeof(clientAddr))) {
         perror("Клиент не получил порт.");
         return 1;
     }
-
     printf("CLIENT: Готов к пересылке.\n\n");
     for (int i = 0; i < number * number; i++) {
-        length = sizeof(clientAddr);
+        length = sizeof(servAddr);
 
         printf("Отправка сообщения: %s\n", argv[3]);
-        
         if (sendto(sock, argv[3], strlen(argv[3]), 0, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0) {
             perror("Проблемы с sendto.\n");
             return 1;
         }
-
-        if ((recvfrom(sock, buf, BUFLEN, 0, (struct sockaddr*)&clientAddr, &length)) < 0) {
-            perror("Плохой socket сервера.");
+        if (recvfrom(sock, buf, BUFLEN, 0, (struct sockaddr*)&servAddr, &length) < 0) {
+            perror("Плохой socket сервера.\n");
             return 1;
         }
-
-        printf("Ответ от сервера: %s\n\n", buf);
-
+        printf("Сообщение от сервера: %s\n\n", buf);
         sleep(number);
     }
     printf("CLIENT: Пересылка завершена. Счастливо оставаться.\n");
--- server.c
+++ server.c
@@ -15,38 +15,35 @@ int main()
     socklen_t length;
     struct sockaddr_in servAddr, clientAddr;
     char buf[BUFLEN];
-
     if ((sockMain = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("Сервер не может открыть socket для UDP.");
         return 1;
     }
-
+    bzero((char*)&servAddr, sizeof(servAddr));
     servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.s_addr = INADDR_ANY;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servAddr.sin_port = 0;
 
     if (bind(sockMain, (struct sockaddr*)&servAddr, sizeof(servAddr))) {
         perror("Связывание сервера неудачно.");
         return 1;
     }
-
     length = sizeof(servAddr);
 
     if (getsockname(sockMain, (struct sockaddr*)&servAddr, &length)) {
         perror("Вызов getsockname неудачен.");
         return 1;
     }
-
-    printf("SERVER: номер порта - %d\n", servAddr.sin_port);
-
+    printf("СЕРВЕР: номер порта - %d\n", ntohs(servAddr.sin_port));
     for (;;) {
         length = sizeof(clientAddr);
+        bzero(buf, BUFLEN);
 
         if ((msgLength = recvfrom(sockMain, buf, BUFLEN, 0, (struct sockaddr*)&clientAddr, &length)) < 0) {
             perror("Плохой socket клиента.");
             return 1;
         }
-        if (sendto(sockMain, buf, BUFLEN, 0, (struct sockaddr*)&clientAddr, length) < 0) {
+        if (sendto(sockMain, buf, BUFLEN, 0, (struct sockaddr*)&clientAddr, sizeof(clientAddr)) < 0) {
             perror("Проблемы с sendto.\n");
             return 1;
         }
